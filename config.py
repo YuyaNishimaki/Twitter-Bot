36f991f7c340b1351559983f34d38a938035c3ff
Yuya Nishimaki <yuya.nishimaki@nislab.doshisha.ac.jp>
2018-12-26 22:29:38 +0900

    fix: using environment variables

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ from dotenv import find_dotenv, load_dotenv
 
 
 load_dotenv(find_dotenv())
-TW_CONSUMER_KEY = os.environ.get("TW_CONSUMER_KEY")
-TW_CONSUMER_SECRET = os.environ.get("TW_CONSUMER_SECRET")
-TW_TOKEN = os.environ.get("TW_TOKEN")
-TW_TOKEN_SECRET = os.environ.get("TW_TOKEN_SECRET")
+TW_CONSUMER_KEY = os.environ.get(
+    "CONSUMER_KEY", os.environ.get("TW_CONSUMER_KEY")
+)  # noqa E501
+TW_CONSUMER_SECRET = os.environ.get(
+    "CONSUMER_SECRET", os.environ.get("TW_CONSUMER_SECRET")
+)
+TW_TOKEN = os.environ.get("ACCESS_TOKEN_KEY", os.environ.get("TW_TOKEN"))
+TW_TOKEN_SECRET = os.environ.get(
+    "ACCESS_TOKEN_SECRET", os.environ.get("TW_TOKEN_SECRET")
+)
